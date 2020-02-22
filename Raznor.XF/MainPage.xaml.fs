fdef946423fc72df708c855f69aecc49851fac9e
Yukun Guo <gyk.net@gmail.com>
2020-02-22 23:30:00 +0800

    Add basic UI

---
+++ Raznor.XF/MainPage.xaml.fs
@@ -0,0 +1,30 @@
+﻿namespace Raznor.XF
+
+open System
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+
+type MainPage() =
+    inherit ContentPage()
+    let _ = base.LoadFromXaml(typeof<MainPage>)
+
+    let songList = base.FindByName<ListView>("songList")
+    let progressBar = base.FindByName<ProgressBar>("progressBar")
+    let openButton = base.FindByName<Button>("openButton")
+    let playButton = base.FindByName<Button>("playButton")
+
+    member this.OnSongTapped(sender: Object, args: EventArgs) =
+        ()
+
+    member this.OnOpenButtonClicked(sender: Object, args: EventArgs) =
+        async {
+            do songList.ItemsSource <- [| "foo"; "bar" |]
+            do playButton.IsEnabled <- true
+            do progressBar.Progress <- 0.0
+        } |> Async.StartImmediate
+
+    member this.OnPlayButtonClicked(sender: Object, args: EventArgs) =
+        async {
+            do playButton.IsEnabled <- false
+            do progressBar.Progress <- 1.0
+        } |> Async.StartImmediate

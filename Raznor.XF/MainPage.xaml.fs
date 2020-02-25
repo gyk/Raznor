a76856f3020025cf5cbe38c0cc0728da75eda649
Yukun Guo <gyk.net@gmail.com>
2020-02-25 23:43:14 +0800

    Support playing local files

--- Raznor.XF/MainPage.xaml.fs
+++ Raznor.XF/MainPage.xaml.fs
@@ -1,8 +1,12 @@
 ﻿namespace Raznor.XF
 
 open System
+open System.Threading.Tasks
+
 open Xamarin.Forms
 open Xamarin.Forms.Xaml
+
+open Plugin.FilePicker
 
 type MainPage() =
     inherit ContentPage()
@@ -18,7 +22,28 @@ type MainPage() =
         "https://upload.wikimedia.org/wikipedia/commons/b/b8/K545_andante.ogg";
     |]
 
+    // TODO: Should be able to open multiple files but the FilePicker plugin does not support it for now.
+    // See https://github.com/jfversluis/FilePicker-Plugin-for-Xamarin-and-Windows/issues/25.
+    // TODO: Extension filters. FilePicker's `allowedTypes` has no cross-platform support.
+    let openFileAsync () =
+        async {
+            let tcs = new TaskCompletionSource<Abstractions.FileData>()
+            Device.BeginInvokeOnMainThread(fun () ->
+                async {
+                    let! fileResult = CrossFilePicker.Current.PickFile() |> Async.AwaitTask
+                    tcs.SetResult(fileResult)
+                } |> Async.StartImmediate
+            )
+            return! tcs.Task |> Async.AwaitTask
+        }
+
     member this.OnItemTapped(sender: Object, args: ItemTappedEventArgs) =
         do
             mediaElement.Source <- MediaSource.op_Implicit(args.Item :?> string)
             mediaElement.Play()
+
+    member this.OnOpenButtonClicked(sender: Object, args: EventArgs) =
+        async {
+            let! fileData = openFileAsync()
+            do songList.ItemsSource <- [| fileData.FilePath |]
+        } |> Async.StartImmediate
